de0e96cd52eb3d30a3d91b8221fc109929c88d59
kershaw-dart <helen.e.kershaw@gmail.com>
2013-04-16 21:03:53 +0000

    writes out transposes
    git-svn-id: https://subversion.ucar.edu/DAReS/DART/branches/helen@6055 acad1b26-7d42-44b9-9cec-aac8293558f5

--- ensemble_manager/TransposeTest/timing.f90
+++ ensemble_manager/TransposeTest/timing.f90
@@ -10,11 +10,10 @@
 !> my_task_id() 0 timings output to standard out
 !> All my_task_id()s timings output to standard error
 !> can use cpu time or MPI_WTIME (namelist flag)
-!> can switch on loop timingsi (MPI_WTIME) with printme=1
 !-----------------------------------------------------      
 program timing
       use mpi_utilities_mod,    only : initialize_mpi_utilities,finalize_mpi_utilities,                &
-                                       my_task_id, task_sync
+                                       my_task_id, task_sync, task_count
       use utilities_mod,        only: find_namelist_in_file, check_namelist_read,                      &
                                       set_nml_output
       use ensemble_manager_mod, only: init_ensemble_manager, ensemble_type, all_vars_to_all_copies,    &
@@ -34,11 +33,11 @@ integer :: n_repeats = 1 !> number of repetitions of loop for a given array size
 integer :: min_array_length = 20 !> minimum number of vars
 integer :: inc_array_length = 10 !> step size for number of vars loop
 integer :: max_array_length = 50 !> max number of vars
-logical :: use_cpu_time = .false. !> default is to use MPI_WTIME
+logical :: record_transpose = .false.
 
 !timing variables
 integer :: array_length !> length of array to transpose
-integer :: i !> for repeats loop
+integer :: i, k, j !> for loops
 real:: cpu_start, cpu_finish !> for cpu_time
 double precision:: start, finish !> for MPI_Wtime
 integer :: printme = 0!> for loop timing inside transpose routines, 0 is off, 1 is on
@@ -48,15 +47,13 @@ integer :: iunit !> for reading in namelist from flle
 integer :: async = 0 !> what does this do for mpi_finalize
 integer :: io !> for iostat
 integer :: stderr = 0 !> for output to standard error
-double precision, allocatable :: memory_splat(:,:) 
 integer :: ierr !> for MPI_BCAST
-
+character*120 :: task_str, file0, file1, file2
 ! namelist : debug flag,  ensemble size (80), number of extras (4 or 6 normally), array length( min, inc, max), number of repitions
 ! use_cpu_time (true/false) 
 namelist /timing_nml/ debug_flag, &
  timing_ens_size, ens_size_extras, &
- min_array_length, inc_array_length, max_array_length, n_repeats, &
- use_cpu_time
+ min_array_length, inc_array_length, max_array_length, n_repeats, record_transpose
 
 ! initialize mpi
  call initialize_mpi_utilities('timing')
@@ -66,22 +63,28 @@ namelist /timing_nml/ debug_flag, &
 
  call check_namelist_read(iunit, io, "timing_nml")
 
+! open output files
+write(task_str, '(i10)') my_task_id()
+file0 = TRIM('inital' // TRIM(ADJUSTL(task_str)) // '.trans')
+file1 = TRIM('intermediate_output' // TRIM(ADJUSTL(task_str)) // '.trans')
+file2 = TRIM('final_output' // TRIM(ADJUSTL(task_str)) // '.trans')
+
+  open(15, file=file0, status ='new')
+  open(20, file=file1, status ='new') ! error if you already have results files
+  open(30, file=file2, status ='new')
+
 ! print run info
- if (my_task_id() == 0) print*, 'ens_size', timing_ens_size, '+ extras', ens_size_extras, 'use_cpu_time', use_cpu_time
+ if (my_task_id() == 0) print*, 'ens_size', timing_ens_size, '+ extras', ens_size_extras
 
 ! print output format to screen
  if (my_task_id() == 0) then
-   print*, 'Timing output: message  array_length  repetition#: time'
-   write(stderr, *) 'Timing output: my_task_id() message array_length repetition# : time'
+   print*, 'Timing output: routine  array_length  repetition#: time'
+   write(stderr, *) 'Timing output: my_task_id() routine array_length repetition# : time'
  end if
  
        numvar : do array_length = min_array_length, max_array_length, inc_array_length
 
             numRep : do i=1, n_repeats
-
-                ! HK loop timing for 
-                 if (i==1) printme =0
-                 if (i==2) printme =0
 
              call task_sync()
 
@@ -91,61 +94,60 @@ namelist /timing_nml/ debug_flag, &
                     if (debug_flag ==1 ) print*, 'inialized ensemble', array_length, i
                 end if     
 
-            ! fill up ensemble
-            ens_handle%my_copies(:) = 42
-           !ens_handles%time(:) ! what to fill this with
-            ens_handle%my_vars(:) = 43
-            ens_handle%vars(:,:) = 44.d0
-            ens_handle%copies(:,:) = 45.d0
-  
-            if (i==1) then
-               if( my_task_id()==1) print *, 'task', my_task_id(), 'num copies = ',  get_my_num_copies(ens_handle), 'my num vars = ', get_my_num_vars(ens_handle)
+            ! set up the ensemble - var complete
+            !   The tasks with ensemble copies are filled
+            do j = 1, ens_handle%my_num_copies
+              do k = 1, ens_handle%num_vars
+                ens_handle%vars(k,j) = j*100000 + (my_task_id() + 1)*1000000 + k
+              enddo
+            enddo
+
+              do j = 1, ens_handle%num_vars
+                write(15, *) ens_handle%vars(j,:)
+              enddo
+    
+
+           if (i==1) then
+               if( my_task_id()==1) then
+                 print *, 'task', my_task_id(), 'num copies = ',  get_my_num_copies(ens_handle), 'my num vars = ', get_my_num_vars(ens_handle)
+                 print *, 'num_vars               = ', array_length
+                 print *, 'ens_size               = ', timing_ens_size + ens_size_extras
+                 print *, 'average message_length = ', array_length / task_count()
+               endif
            endif
 
             call task_sync()
-
-          if (use_cpu_time .eqv. .true.) then
-   
-         ! call transpose all_vars_to_all_copies
-                    call cpu_time(start)
-                    call all_vars_to_all_copies(ens_handle,printme)
-                    call cpu_time(finish)
-                    if (my_task_id() == 0 ) print *, 'c all_vars_to_all_copies ', array_length, i,' : ', finish - start
-                   write(stderr, *)my_task_id(),' vars_to_copies',  array_length, i,' : ', finish - start
-
-                call task_sync()
-  
-        ! call transpose all_copies_to_all_vars
- 
-                    call cpu_time(start)
-                    call all_copies_to_all_vars(ens_handle, printme)
-                    call cpu_time(finish)
-                    if (my_task_id() == 0 ) print *, 'c all_copies_to_all_vars ', array_length, i,' : ', finish - start
-                    write(stderr, *)my_task_id(),' copies_to_vars',  array_length, i,' : ', finish - start
-  
-              else   
 
           ! call transpose all_vars_to_all_copies
                     start = MPI_WTIME() 
                    call all_vars_to_all_copies(ens_handle,printme)
                     finish = MPI_WTIME() 
-                    if (my_task_id() == 0 ) print *, 'c all_vars_to_all_copies ', array_length, i,' : ', finish - start
+                    if (my_task_id() == 0 ) print *, 'all_vars_to_all_copies ', array_length, i,' : ', finish - start
                     write(stderr, *)my_task_id(),' vars_to_copies',  array_length, i,' : ', finish - start
 
-                call task_sync()
+          ! write transpose results to file
+              do k = 1, ens_handle%num_copies
+                write(20, *) ens_handle%copies(k,:)
+              enddo
+    
+            call task_sync()
 
-         ! call transpose all_copies_to_all_vars
-                   start = MPI_WTIME()  
+          ! call transpose all_copies_to_all_vars
+                   start = MPI_WTIME()
                    call all_copies_to_all_vars(ens_handle, printme)
                    finish = MPI_WTIME() 
-                   if (my_task_id() == 0 ) print *, 'c all_copies_to_all_vars ', array_length, i,' : ', finish - start
+                   if (my_task_id() == 0 ) print *, 'all_copies_to_all_vars ', array_length, i,' : ', finish - start
                    write(stderr, *)my_task_id(),' copies_to_vars',  array_length, i,' : ', finish - start
 
-           endif
 
            call task_sync()
 
-           ! destroy storage 
+         ! write transpose results to file
+          do j = 1, ens_handle%num_vars
+            write(30, *) ens_handle%vars(j, :)
+          enddo
+
+         ! destroy storage
           call end_ensemble_manager(ens_handle)
          if (my_task_id() == 0 ) then
           if (debug_flag ==1 ) print*, 'destroyed ensemble_storage ', array_length, i
@@ -154,6 +156,10 @@ namelist /timing_nml/ debug_flag, &
         end do numRep
 
     end do numvar
+
+  close(15)
+  close(20)
+  close(30)
 
 ! finialize mpi
  call finalize_mpi_utilities(async=async)
